1140bc866b97b2fd8cf77a1832101cf6923cb54e
Zenus <75637878+zenusop@users.noreply.github.com>
2025-01-31 04:32:04 -0800

    Initialize 1.12

---
+++ src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "examplemod", useMetadata=true)
+public class ExampleMod {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Dirt: " + Blocks.DIRT.getTranslationKey());
+	    System.out.println("Color State: " + new GlStateManager.Color());
+    }
+}
